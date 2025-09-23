dce41034c0d540d85836c65cb1bef8c67b3b1ae2
Meghag231 <133561322+Meghag231@users.noreply.github.com>
2025-09-23 13:11:19 +0530

    Update DashboardPage.java

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -2,17 +2,28 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class DashboardPage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     private By dashboardHeading = By.xpath("//h6[text()='Dashboard']");
+    private By pimMenu = By.xpath("//span[text()='PIM']");
 
     public DashboardPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
     public String getDashboardHeading() {
-        return driver.findElement(dashboardHeading).getText();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardHeading)).getText();
+    }
+
+    public EmployeePage goToEmployeePage() {
+        wait.until(ExpectedConditions.elementToBeClickable(pimMenu)).click();
+        return new EmployeePage(driver);
     }
 }
