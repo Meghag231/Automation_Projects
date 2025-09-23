f30ffa0204d0a489627f94002a150b7a3efca5c3
Meghag231 <133561322+Meghag231@users.noreply.github.com>
2025-09-23 15:17:03 +0530

    Update DashboardTest.java

--- src/test/java/Tests/DashboardTest.java
+++ src/test/java/Tests/DashboardTest.java
@@ -1,27 +1,19 @@
 package tests;
 
+import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import base.BaseTest;
-import pages.DashboardPage;
 import pages.LoginPage;
+import pages.DashboardPage;
 
-public class DashboardTest extends BaseTest
-{
-	@Test
-	public void verifyDashboardAfterLogin()
-	{
-		LoginPage loginPage=new LoginPage(driver);
-		DashboardPage dashboardPage=loginPage.login("Admin", "admin123");
-		
-		String heading = dashboardPage.getDashboardHeading();
-		
-		Assert.assertEquals(heading,"Dashboard","Dashboard heading mismatch");
-		
-		
-		
-	}
+public class DashboardTest extends BaseTest {
 
+    @Test
+    public void verifyDashboardAfterLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        DashboardPage dashboardPage = loginPage.login("Admin", "admin123");
+
+        String heading = dashboardPage.getDashboardHeading();
+        Assert.assertEquals(heading, "Dashboard", "Dashboard heading mismatch!");
+    }
 }
-
